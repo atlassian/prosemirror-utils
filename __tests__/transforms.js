86c6ce139acd211665e746fdcf5a525b5b20fea6
Eduard Shvedai <eshvedai@atlassian.com>
2018-03-09 17:26:28 +1100

    added replaceParentNodeOfTypeWith helper

--- __tests__/transforms.js
+++ __tests__/transforms.js
@@ -1,6 +1,7 @@
 import { createEditor, toEqualDocument, doc, p, table, tr as row, td, th, tdCursor, tdEmpty } from '../test-helpers';
 import {
   removeParentNodeOfType,
+  replaceParentNodeOfTypeWith,
 } from '../src';
 
 describe('transforms', () => {
@@ -18,4 +19,36 @@ describe('transforms', () => {
       });
     });
   });
+
+  describe('replaceParentNodeOfTypeWith', () => {
+    it('should return `undefined` if there is no parent node of a given NodeType', () => {
+      const { state } = createEditor(doc(p('<cursor>')));
+      const node = state.schema.nodes.paragraph.createChecked({}, state.schema.text('new'));
+      const result = replaceParentNodeOfTypeWith(state.schema.nodes.table, node)(state);
+      expect(result).toBeUndefined();
+    });
+    it('should return `undefined` if replacing is not possible', () => {
+      const { state } = createEditor(doc(p('one'), table(row(tdCursor)), p('two')));
+      const node = state.schema.text('new');
+      const result = replaceParentNodeOfTypeWith(state.schema.nodes.table, node)(state);
+      expect(result).toBeUndefined();
+    });
+    describe('when there is a p("one") before the table node and p("two") after', () => {
+      it('should replace table with p("new") and preserve p("one") and p("two")', () => {
+        const { state } = createEditor(doc(p('one'), table(row(tdCursor)), p('two')));
+        const node = state.schema.nodes.paragraph.createChecked({}, state.schema.text('new'));
+        const tr = replaceParentNodeOfTypeWith(state.schema.nodes.table, node)(state);
+        toEqualDocument(tr.doc, doc(p('one'), p('new'), p('two')));
+      });
+    });
+    describe('when there are tree paragraphs', () => {
+      it('should replace the middle paragraph with p("new") and preserve p("one") and p("two")', () => {
+        const { state } = createEditor(doc(p('one'), p('hello<cursor>there'), p('two')));
+        const node = state.schema.nodes.paragraph.createChecked({}, state.schema.text('new'));
+        const tr = replaceParentNodeOfTypeWith(state.schema.nodes.paragraph, node)(state);
+        toEqualDocument(tr.doc, doc(p('one'), p('new'), p('two')));
+      });
+    });
+  });
+
 });
--- src/transforms.js
+++ src/transforms.js
@@ -11,3 +11,17 @@ export const removeParentNodeOfType = (nodeType) => (state) => {
     return state.tr.delete(from, to);
   }
 }
+
+// :: (nodeType: NodeType, node: ProseMirrorNode) → (state: EditorState) → ?Transaction
+// Returns a `replace` transaction that replaces parent node of a given `nodeType` with the given `node`.
+export const replaceParentNodeOfTypeWith = (nodeType, node) => (state) => {
+  const parent = findParentNodeOfType(nodeType)(state);
+  if (parent) {
+    const $pos = state.doc.resolve(parent.pos);
+    const from = $pos.before($pos.depth);
+    const to = $pos.after($pos.depth);
+    if (state.doc.canReplaceWith($pos.index($pos.depth), $pos.indexAfter($pos.depth), node.type)) {
+      return state.tr.replaceWith(from, to, node);
+    }
+  }
+}
