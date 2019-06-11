0410570591973f4331beaf00f0c2aae6c9634e07
Eduard Shvedai <eshvedai@atlassian.com>
2019-06-11 12:03:21 +1000

    fix: setting NodeSelection on the inserted node if its selectable

--- src/transforms.js
+++ src/transforms.js
@@ -113,6 +113,15 @@ export const setTextSelection = (position, dir = 1) => tr => {
   return tr;
 };
 
+const isSelectableNode = node => node.type && node.type.spec.selectable;
+
+const setSelection = (node, pos, tr) => {
+  if (isSelectableNode(node)) {
+    return tr.setSelection(new NodeSelection(tr.doc.resolve(pos)));
+  }
+  return setTextSelection(pos)(tr);
+};
+
 // :: (content: union<ProseMirrorNode, Fragment>, position: ?number, tryToReplace?: boolean) → (tr: Transaction) → Transaction
 // Returns a new transaction that inserts a given `content` at the current cursor position, or at a given `position`, if it is allowed by schema. If schema restricts such nesting, it will try to find an appropriate place for a given node in the document, looping through parent nodes up until the root document node.
 // If `tryToReplace` is true and current selection is a NodeSelection, it will replace selected node with inserted content if its allowed by schema.
@@ -133,7 +142,7 @@ export const safeInsert = (content, position, tryToReplace) => tr => {
     : isNodeSelection(tr.selection)
     ? tr.doc.resolve($from.pos + 1)
     : $from;
-  const { parent, depth } = $insertPos;
+  const { parent } = $insertPos;
 
   // try to replace selected node
   if (isNodeSelection(tr.selection) && tryToReplace) {
@@ -149,18 +158,24 @@ export const safeInsert = (content, position, tryToReplace) => tr => {
     const oldTr = tr;
     tr = replaceParentNodeOfType(parent.type, content)(tr);
     if (oldTr !== tr) {
-      return setTextSelection($insertPos.pos)(tr);
+      const pos = isSelectableNode(content)
+        ? // for selectable node, selection position would be the position of the replaced parent
+          $insertPos.before($insertPos.depth)
+        : $insertPos.pos;
+      return setSelection(content, pos, tr);
     }
   }
 
   // given node is allowed at the current cursor position
   if (canInsert($insertPos, content)) {
     tr.insert($insertPos.pos, content);
-    return cloneTr(
-      setTextSelection(hasPosition ? $insertPos.pos : tr.selection.$anchor.pos)(
-        tr
-      )
-    );
+    const pos = hasPosition
+      ? $insertPos.pos
+      : isSelectableNode(content)
+      ? // for atom nodes selection position after insertion is the previous pos
+        tr.selection.$anchor.pos - 1
+      : tr.selection.$anchor.pos;
+    return cloneTr(setSelection(content, pos, tr));
   }
 
   // looking for a place in the doc where the node is allowed
@@ -169,7 +184,7 @@ export const safeInsert = (content, position, tryToReplace) => tr => {
     const $pos = tr.doc.resolve(pos);
     if (canInsert($pos, content)) {
       tr.insert(pos, content);
-      return cloneTr(setTextSelection(pos)(tr));
+      return cloneTr(setSelection(content, pos, tr));
     }
   }
   return tr;
