93d8b400e522ef30e3f7700946c112043daf1ab7
Brian Hung <brianhung@berkeley.edu>
2020-07-15 17:45:50 -0700

    add esm as an output format

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,36 @@
 import babel from 'rollup-plugin-babel';
 
-export default {
-  input: './src/index.js',
-  output: {
-    format: 'cjs',
-    file: 'dist/index.js',
-    sourcemap: true
+export default [
+  {
+    input: './src/index.js',
+    output: {
+      format: 'cjs',
+      file: 'dist/index.js',
+      sourcemap: true
+    },
+    plugins: [
+      babel({
+        exclude: 'node_modules/**'
+      })
+    ],
+    external(id) {
+      return !/^[\.\/]/.test(id);
+    }
   },
-  plugins: [
-    babel({
-      exclude: 'node_modules/**'
-    })
-  ],
-  external(id) {
-    return !/^[\.\/]/.test(id);
+  {
+    input: './src/index.js',
+    output: {
+      format: 'es',
+      file: 'dist/index.esm.js',
+      sourcemap: true
+    },
+    plugins: [
+      babel({
+        exclude: 'node_modules/**'
+      })
+    ],
+    external(id) {
+      return !/^[\.\/]/.test(id);
+    }
   }
-};
+];
