876c289f086f4bde589061180cf5d7ebfe0598e5
Rodrigo Vieira <rvieira@atlassian.com>
2023-07-14 13:48:23 +1000

    Remove export * from index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,36 @@
-export * from './selection';
-export * from './node';
-export * from './transforms';
+export {
+  findParentNode,
+  findParentNodeClosestToPos,
+  findParentDomRef,
+  hasParentNode,
+  findParentNodeOfType,
+  findParentNodeOfTypeClosestToPos,
+  hasParentNodeOfType,
+  findParentDomRefOfType,
+  findSelectedNodeOfType,
+  findPositionOfNodeBefore,
+  findDomRefAtPos,
+} from './selection';
+export {
+  flatten,
+  findChildren,
+  findTextNodes,
+  findInlineNodes,
+  findBlockNodes,
+  findChildrenByAttr,
+  findChildrenByType,
+  findChildrenByMark,
+  contains,
+} from './node';
+export {
+  removeParentNodeOfType,
+  replaceParentNodeOfType,
+  removeSelectedNode,
+  replaceSelectedNode,
+  setTextSelection,
+  safeInsert,
+  setParentNodeMarkup,
+  selectParentNodeOfType,
+  removeNodeBefore,
+} from './transforms';
 export { isNodeSelection, canInsert } from './helpers';
